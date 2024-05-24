763545fefe7d9982e0b9614adff25f0bc0468612
soo <ksbsj92@naver.com>
2024-05-24 12:38:03 +0900

    # REST API 4 - log

--- src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
@@ -4,6 +4,7 @@ import com.study.springstudy.springmvc.chap05.dto.response.ReplyDetailDto;
 import com.study.springstudy.springmvc.chap05.entity.Reply;
 import com.study.springstudy.springmvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/replies")
 @RequiredArgsConstructor
+@Slf4j
 public class ReplyApiController {
 
     private final ReplyService replyService;
@@ -22,8 +24,18 @@ public class ReplyApiController {
     @GetMapping("/{bno}")
     public ResponseEntity<?> list(@PathVariable long bno) {
 //        System.out.println("bno = " + bno);
+        if(bno == 0) {
+            String message =  "글 번호는 0번이 될 수 없습니다.";
+            log.warn(message);
+            return ResponseEntity
+                    .badRequest()
+                    .body(message);
+        }
+
+        log.info("/api/v1/replies/{} : GET" , bno);
 
         List<ReplyDetailDto> replies = replyService.getReplies(bno);
+        log.debug("first reply: {}", replies.get(0));
 
         return ResponseEntity
                 .ok()
