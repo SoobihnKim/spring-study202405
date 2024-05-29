edeabcf953471391cc653581829b29ce665b3df1
soo <ksbsj92@naver.com>
2024-05-29 14:55:09 +0900

    # 회원관리 시스템 - Entity, Mapper 생성

--- src/main/resources/board.sql
+++ src/main/resources/board.sql
@@ -85,3 +85,15 @@ WHERE reply_no = 2;
 
 SELECT * FROM tbl_reply
 WHERE board_no = 100;
+
+
+-- 회원 관리 테이블
+CREATE TABLE tbl_member (
+    account VARCHAR(50),
+    password VARCHAR(150) NOT NULL, -- 암호화해야해서 넉넉하게
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    auth VARCHAR(20) DEFAULT 'COMMON',
+    reg_date DATETIME DEFAULT current_timestamp,
+        CONSTRAINT pk_member PRIMARY KEY (account)
+);
